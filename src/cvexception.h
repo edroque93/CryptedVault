ab96322b7d1eb7d7575bdcbf0587eaeec00e68c4
edroque93 <quique.lpgc@gmail.com>
2020-02-06 20:13:21 +0100

    Missed exception file

---
+++ src/cvexception.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <sstream>
+
+namespace CryptedVault
+{
+    template <class T>
+    class CryptedVaultException : public std::exception
+    {
+        public:
+            CryptedVaultException(T code, std::string_view details) 
+                : details(details)
+                , code(code) 
+            { 
+                stream << "Exception: " << code << " (" << details << ").";
+                whatMessage = stream.str();
+            }
+
+            CryptedVaultException(T code) 
+                : code(code)
+            { 
+                stream << "Exception: " << code << ".";
+                whatMessage = stream.str();
+            }
+
+            const char *what() const throw ()
+            {
+                return  whatMessage.c_str();
+            }
+
+            T getCode() { return code; }
+            std::string getDetails() { return details; }
+
+        private:
+            std::string details;
+            T code;
+            std::stringstream stream;
+            std::string whatMessage;
+    };
+}
