31816aaca328e05b0caca8ee3091b6b7b17286ec
edroque93 <quique.lpgc@gmail.com>
2020-02-12 17:56:37 +0100

    Dynamic widgets seem to be working fine, it definetly looks better than the grid approach...

--- src/window.cpp
+++ src/window.cpp
@@ -11,6 +11,8 @@ namespace CryptedVault::UI
             wxDefaultSize,
             wxDEFAULT_FRAME_STYLE & ~(wxMAXIMIZE_BOX | wxRESIZE_BORDER))
         , mainSizer(nullptr)
+        , scrolledWindow(nullptr)
+        , readOnly(nullptr)
         , handCursorWasEnabled(false)
     {
         SetMinSize(wxSize(700, 350));
@@ -18,8 +20,9 @@ namespace CryptedVault::UI
 
         mainSizer = new wxBoxSizer(wxVERTICAL);
 
-        generateVaultView();
-        // generateEmptyView();
+        generateEmptyView();
+        
+        setReadOnly(true);
 
         Bind(wxEVT_MOTION, &Window::mouseOverFrame, this, wxID_ANY);
 
@@ -41,8 +44,8 @@ namespace CryptedVault::UI
         text3->SetFont(wxFontInfo(26).Bold());
         text4->SetFont(wxFontInfo(26));
 
-        text1->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent &event) { OpenVaultFile(); }, wxID_ANY);
-        text3->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent &event) { CreateVaultFile(); }, wxID_ANY);
+        text1->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent &event) { openVaultFile(); }, wxID_ANY);
+        text3->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent &event) { createVaultFile(); }, wxID_ANY);
         text1->Bind(wxEVT_MOTION, &Window::mouseOverLabels, this, wxID_ANY);
         text2->Bind(wxEVT_MOTION, &Window::mouseOverFrame, this, wxID_ANY);
         text3->Bind(wxEVT_MOTION, &Window::mouseOverLabels, this, wxID_ANY);
@@ -56,6 +59,8 @@ namespace CryptedVault::UI
         mainSizer->AddStretchSpacer();
         mainSizer->Add(textSizer, 0, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
         mainSizer->AddStretchSpacer();
+
+        mainSizer->Layout();
     }
 
     void Window::testEvent(wxMouseEvent &event)
@@ -81,36 +86,135 @@ namespace CryptedVault::UI
         }
     }
 
-
     void Window::generateVaultView()
     {
         mainSizer->Clear(true);
 
-        wxScrolledWindow *scrolledWindow = new wxScrolledWindow(this);
+        mainSizer->Add(buildHeader(), 0, wxALL | wxEXPAND, 5);
+
+        scrolledWindow = new wxScrolledWindow(this);
         scrolledWindow->SetScrollbars(10, 10, 10, 10);
         scrolledWindow->ShowScrollbars(wxScrollbarVisibility::wxSHOW_SB_NEVER, wxScrollbarVisibility::wxSHOW_SB_DEFAULT);
-
-        wxBoxSizer *customGrid = new wxBoxSizer(wxVERTICAL);
-        scrolledWindow->SetSizer(customGrid);
+        scrolledWindow->SetSizer(new wxBoxSizer(wxVERTICAL));
 
         mainSizer->Add(scrolledWindow, 1, wxALL | wxGROW);
 
-        for (int  i = 0; i < 2; i++)
-        {
-            wxButton *b = new wxButton(scrolledWindow, wxID_ANY, "Hey!");
-            customGrid->Add(b, 0, wxALL, 10);
-        }
-
         wxBoxSizer *options = new wxBoxSizer(wxHORIZONTAL);
-        wxButton *button = new wxButton(this, wxID_ANY, "Add");
-        button->Bind(wxEVT_LEFT_DOWN, [this, scrolledWindow, customGrid](wxMouseEvent &event) { 
-            auto sizer = scrolledWindow->GetSizer();
-            wxButton *b = new wxButton(scrolledWindow, wxID_ANY, "Hey!");
-            sizer->Add(b, 0, wxALL, 10);
-            scrolledWindow->FitInside();
-        }, wxID_ANY);
+        wxButton *button = new wxButton(this, wxID_ADD);
+        button->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent &event) { addEntry("", "", "", ""); });
+        readOnly = new wxCheckBox(this, wxID_ANY, "Read only");
+        readOnly->SetValue(true);
+        readOnly->Bind(wxEVT_CHECKBOX, [this](wxCommandEvent &event) { 
+            setReadOnly(event.IsChecked(), false);
+        });
+        wxCheckBox *viewPassword = new wxCheckBox(this, wxID_ANY, "View password");
+        viewPassword->SetValue(true);
+        viewPassword->Enable(false);
+
+        options->Add(viewPassword, 0, wxALL | wxALIGN_CENTER_VERTICAL, 10);
+        options->Add(readOnly, 0, wxALL | wxALIGN_CENTER_VERTICAL, 10);
         options->Add(button, 0, wxALL, 10);
         mainSizer->Add(options, 0, wxALIGN_RIGHT);
+        
+        mainSizer->Layout();
+    }
+
+    void Window::setReadOnly(bool value, bool setUI)
+    {
+        if (scrolledWindow == nullptr || readOnly == nullptr) return;
+        for (auto &child : scrolledWindow->GetChildren())
+        {
+            child->Enable(!value);
+        }
+        if (setUI)
+        {
+            readOnly->SetValue(value);
+        }
+    }
+
+    void Window::addEntry(std::string domain, std::string user, std::string password, std::string comment)
+    {
+        if (scrolledWindow == nullptr) return;
+        if (readOnly == nullptr) return;
+
+        wxSizer *sizerOfScrolledWindow = scrolledWindow->GetSizer();
+        wxBoxSizer *entrySizer = new wxBoxSizer(wxHORIZONTAL);
+                
+        wxTextCtrl *textDomain = new wxTextCtrl(scrolledWindow, ctrlID::ID_DOMAIN);
+        wxTextCtrl *textUser = new wxTextCtrl(scrolledWindow, ctrlID::ID_USER);
+        wxTextCtrl *textPassword = new wxTextCtrl(scrolledWindow, ctrlID::ID_PASSWORD);
+        wxTextCtrl *textComment = new wxTextCtrl(scrolledWindow, ctrlID::ID_COMMENT);
+        wxButton *removeButton = new wxButton(scrolledWindow, wxID_ANY);
+        removeButton->Bind(wxEVT_LEFT_DOWN, [this, entrySizer, sizerOfScrolledWindow, textDomain](wxMouseEvent &event) {
+            if (wxMessageDialog(
+                    this, 
+                    "Are you sure you want to delete the entry for \"" + textDomain->GetValue().ToStdString() + "\"?",
+                    "Delete", 
+                    wxYES_NO | wxNO_DEFAULT | wxICON_ASTERISK
+                ).ShowModal() == wxID_NO) 
+            {
+                return;
+            }
+
+            uiEntries.erase(std::remove(uiEntries.begin(), uiEntries.end(), entrySizer));
+            sizerOfScrolledWindow->Hide(entrySizer);
+            sizerOfScrolledWindow->Layout();
+            scrolledWindow->Layout();
+            scrolledWindow->SetScrollbars(10, 10, 10, 10);
+        });
+
+        textDomain->SetMinSize(wxSize(160, -1));
+        textUser->SetMinSize(wxSize(130, -1));
+        textPassword->SetMinSize(wxSize(130, -1));
+        textComment->SetMinSize(wxSize(175, -1));
+        removeButton->SetMinSize(wxSize(30, textComment->GetSize().y));
+
+        removeButton->SetBitmap(wxArtProvider::GetBitmap(wxART_DELETE));
+
+        textDomain->SetValue(domain);
+        textUser->SetValue(user);
+        textPassword->SetValue(password);
+        textComment->SetValue(comment);
+
+        entrySizer->Add(5, -1);
+        entrySizer->Add(textDomain, 0, wxALL, 5);
+        entrySizer->Add(textUser, 0, wxALL, 5);
+        entrySizer->Add(textPassword, 0, wxALL, 5);
+        entrySizer->Add(textComment, 0, wxALL, 5);
+        entrySizer->Add(removeButton, 0, wxALL, 5);
+
+        uiEntries.push_back(entrySizer);
+        sizerOfScrolledWindow->Add(entrySizer, 0, wxALL);
+        scrolledWindow->FitInside();
+        scrolledWindow->Scroll(wxPoint(entrySizer->GetPosition().x, entrySizer->GetPosition().y));
+
+        setReadOnly(false);
+    }
+
+    wxBoxSizer *Window::buildHeader()
+    {
+        wxBoxSizer *headerSizer = new wxBoxSizer(wxHORIZONTAL);
+        wxStaticText *header1 = new wxStaticText(this, wxID_ANY, "Domain");
+        wxStaticText *header2 = new wxStaticText(this, wxID_ANY, "User");
+        wxStaticText *header3 = new wxStaticText(this, wxID_ANY, "Password");
+        wxStaticText *header4 = new wxStaticText(this, wxID_ANY, "Comment");
+        
+        header1->SetFont(wxFontInfo(14).Bold());
+        header2->SetFont(wxFontInfo(14).Bold());
+        header3->SetFont(wxFontInfo(14).Bold());
+        header4->SetFont(wxFontInfo(14).Bold());
+
+        header1->SetMinSize(wxSize(160, -1));
+        header2->SetMinSize(wxSize(130, -1));
+        header3->SetMinSize(wxSize(130, -1));
+        header4->SetMinSize(wxSize(175, -1));
+
+        headerSizer->Add(header1, 0, wxALL, 5);
+        headerSizer->Add(header2, 0, wxALL, 5);
+        headerSizer->Add(header3, 0, wxALL, 5);
+        headerSizer->Add(header4, 0, wxALL, 5);
+
+        return headerSizer;
     }
 
     void Window::buildMenu()
@@ -127,8 +231,8 @@ namespace CryptedVault::UI
         menuBar->Append(menuHelp, "&Help");
         SetMenuBar( menuBar );
 
-        Bind(wxEVT_MENU, [this](wxCommandEvent &event) { CreateVaultFile(); }, wxID_NEW);
-        Bind(wxEVT_MENU, [this](wxCommandEvent &event) { OpenVaultFile(); }, wxID_OPEN);
+        Bind(wxEVT_MENU, [this](wxCommandEvent &event) { createVaultFile(); }, wxID_NEW);
+        Bind(wxEVT_MENU, [this](wxCommandEvent &event) { openVaultFile(); }, wxID_OPEN);
         Bind(wxEVT_MENU, 
             [](wxCommandEvent &event) { 
                 wxMessageBox("Store your passwords in a safe vault!", "About", wxOK | wxICON_INFORMATION); 
@@ -137,7 +241,7 @@ namespace CryptedVault::UI
         Bind(wxEVT_MENU, [this](wxCommandEvent &event) { Close(true); }, wxID_EXIT);
     }
 
-    void Window::OpenVaultFile()
+    void Window::openVaultFile()
     {
         wxFileDialog openFileDialog(this, "Open vault file", "", "", "CVF files (*.cvf)|*.cvf|Any file (*)|*", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
         if (openFileDialog.ShowModal() == wxID_CANCEL) return;
@@ -161,7 +265,7 @@ namespace CryptedVault::UI
         }
     }
 
-    void Window::CreateVaultFile()
+    void Window::createVaultFile()
     {
         wxFileDialog saveFileDialog(this, "New vault file", "", "", "CVF files (*.cvf)|*.cvf|Any file (*)|*", wxFD_SAVE);
         saveFileDialog.SetFilename("vault.cvf");
@@ -178,5 +282,7 @@ namespace CryptedVault::UI
                 wxMessageBox("Cannot set an empty password", "Error", wxOK | wxICON_ERROR, this);
             }
         }
+
+        generateVaultView();
     }
 }
